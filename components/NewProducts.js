df094fb8ebf22f98289eabd3331b9f657ff2409b
Tom <tomek286@yahoo.com>
2024-11-11 13:10:28 +0100

    style

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -19,7 +19,7 @@ export default function NewProducts({ products }) {
         <>
             <section className="items-center py-4 md:absolute relative right-4 top-20 p-12 bg-slate-100 dark:bg-slate-800 text-black dark:text-white">
 
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-24">
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-12">
                     <h1 className="text-center font-manrope font-bold text-4xl text-black dark:text-slate-400 mb-8 max-xl:text-center">Originální náhradní díly pro Mercedes-Benz</h1>
                     <h2 className="text-center mb-4 dark:text-slate-400">
                         Nabízíme<br />
@@ -37,12 +37,12 @@ export default function NewProducts({ products }) {
 
                                 <Image className="rounded-2xl object-cover" src={product.images[0]} width={200} height={200} />
                                 <div
-                                    className="absolute z-10 bottom-3 left-0 mx-3 p-3 bg-white w-[calc(100%-24px)] rounded-xl shadow-sm shadow-transparent transition-all duration-500 group-hover:shadow-indigo-200 group-hover:bg-indigo-50">
+                                    className="absolute z-10 bottom-3 left-0 mx-3 p-3 bg-slate-300 bg-opacity-50 w-[calc(100%-24px)] rounded-xl shadow-sm shadow-transparent transition-all duration-500 group-hover:shadow-indigo-200 group-hover:bg-indigo-50">
                                     <div className="flex items-center justify-between mb-2">
                                         <h6 className="font-semibold text-base leading-7 text-black ">{product.model}</h6>
                                         <h6 className="font-semibold text-base leading-7 text-indigo-600 text-right">{product.price} Kč</h6>
                                     </div>
-                                    <p className="text-xs leading-5 text-gray-500">{product.title}</p>
+                                    <p className="text-xs leading-5 text-gray-500">{product.title.slice(0,50)}...</p>
                                 </div>
                             </div>
                         ))}

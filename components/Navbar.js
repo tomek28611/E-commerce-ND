b16eec71f88d80357321eb7eef5a199804fdb2c4
Tom <tomek286@yahoo.com>
2024-11-13 11:29:41 +0100

    nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { FaMoon, FaSun } from 'react-icons/fa';
 import Link from 'next/link';
+import Spinner from "@/components/Spinner";
+import { useRouter } from 'next/router';
 
 export default function Navbar({ toggleTheme, theme }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  
+  const handleRouteChangeStart = () => setLoading(true);
+  const handleRouteChangeComplete = () => setLoading(false);
+
+ 
+  router.events.on('routeChangeStart', handleRouteChangeStart);
+  router.events.on('routeChangeComplete', handleRouteChangeComplete);
+  router.events.on('routeChangeError', handleRouteChangeComplete);
+
   return (
     <nav className="flex items-center justify-between p-4 shadow-md bg-slate-200 dark:bg-gray-800">
-      {/* Logo */}
-      
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <Spinner />
+        </div>
+      )}
+
       <div className="text-2xl font-bold text-black dark:text-white">
         <Link href="/">
           <div className="hover:text-blue-500">Logo</div>
         </Link>
       </div>
 
-      {/* Menu */}
       <ul className="flex space-x-6">
         <li>
           <Link href="/">
@@ -25,26 +43,27 @@ export default function Navbar({ toggleTheme, theme }) {
           </Link>
         </li>
         <li>
-          <Link href="/">
-            <div className="text-black dark:text-white hover:text-blue-500">Home</div>
+          <Link href="/podpora">
+            <div className="text-black dark:text-white hover:text-blue-500">Podpora</div>
           </Link>
         </li>
         <li>
-          <Link href="/">
+          <Link href="/kontakt">
             <div className="text-black dark:text-white hover:text-blue-500">Kontakt</div>
           </Link>
         </li>
       </ul>
 
-    
-      <div className="hidden md:flex items-center space-x-4  ">
+      <div className="hidden md:flex items-center space-x-4">
         <input
           type="text"
           placeholder="Search..."
           className="px-2 py-1 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 dark:text-white focus:outline-none"
         />
         <button
-          onClick={toggleTheme}
+          onClick={() => {
+            toggleTheme();
+          }}
           className="p-2 bg-gray-200 dark:bg-gray-600 rounded-full hover:bg-gray-300 dark:hover:bg-gray-500"
         >
           {theme === 'light' ? <FaMoon className="text-black" /> : <FaSun className="text-white" />}

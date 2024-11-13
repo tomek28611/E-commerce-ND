9e5c140e0e4ec25f400fb84d6387bf90962b1f03
Tom <tomek286@yahoo.com>
2024-11-13 10:19:08 +0100

    meta

--- pages/index.js
+++ pages/index.js
@@ -22,27 +22,29 @@ export default function HomePage({ featuredProduct, newProducts }) {
     <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Autodíly pro Mercedes – Rychlé a spolehlivé dodání </title>
-        <meta
-          name="description"
-          content="Objevte širokou nabídku kvalitních autodílů pro vozy Mercedes. Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka, skvělé ceny a spolehlivý servis. Vaše auto si zaslouží to nejlepší!"
-        />
+        <title>Originální náhradní díly pro Mercedes-Benz – Kvalitní autodíly </title>
+        <meta name="description" content="Originální náhradní díly pro Mercedes-Benz. Kvalitní autodíly, spolehlivé a kompatibilní, pro váš vůz Mercedes-Benz." />
 
-        <link rel="cannonical" href="https://autodilyexpress.cz" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/auto-dily-logo.png" />
+
+
+        <link rel="cannonical" href="https://www.nahradni-dily.com/" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://www.nahradni-dily.com/logo-nerozza.png" />
         <meta property="og:locale" content="cs_CZ" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
-        <meta property="og:image" content="/auto-dily-logo.png" />
-        <meta property="og:image:width" content="50" />
-        <meta property="og:image:height" content="50" />
+        <meta property="og:title" content="Originální náhradní díly pro Mercedes-Benz. Kvalitní autodíly, spolehlivé a kompatibilní, pro váš vůz Mercedes-Benz." />
+        <meta property="og:image" content="https://www.nahradni-dily.com/logo-nerozza.png" />
+        <meta property="og:image:alt" content="Náhradní díly pro Mercedes-Benz" />
+        <meta property="og:image:width" content="150" />
+        <meta property="og:image:height" content="150" />
         <meta property="og:image:type" content="image/png" />
-        <meta property="og:description" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
-        <meta property="og:url" content="https://autodilyexpress.cz" />
-        <meta property="og:site_name" content="autodilyexpress.cz" />
+        <meta property="og:description" content="Originální náhradní díly pro Mercedes-Benz. Kvalitní autodíly, spolehlivé a kompatibilní, pro váš vůz Mercedes-Benz." />
+        <meta property="og:url" content="https://www.nahradni-dily.com/" />
+        <meta property="og:site_name" content="nahradni-dily.com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
-        <meta name="twitter:description" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka"></meta>
+        <meta name="twitter:title" content="Originální náhradní díly pro Mercedes-Benz. Kvalitní autodíly, spolehlivé a kompatibilní, pro váš vůz Mercedes-Benz." />
+        <meta name="twitter:image" content="https://www.nahradni-dily.com/logo-nerozza.png"/>
+          <meta name="twitter:image:alt" content="Náhradní díly pro Mercedes-Benz"></meta>
+          <meta name="twitter:description" content="Originální náhradní díly pro Mercedes-Benz. Kvalitní autodíly, spolehlivé a kompatibilní, pro váš vůz Mercedes-Benz."></meta>
 
       </Head>
       <div className="bg-white dark:bg-black text-black dark:text-white">
@@ -52,7 +54,7 @@ export default function HomePage({ featuredProduct, newProducts }) {
           <div className="new_prod_cont">
             <NewProducts products={newProducts} productF={featuredProduct} />
           </div>
-          
+
         </div>
 
       </div>
